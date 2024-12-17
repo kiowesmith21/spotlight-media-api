4977558f29dc963131efb7d321357585d02d8a8f
kiowesmith21 <kiowesmith@gmail.com>
2024-12-17 16:26:40 -0500

    refactor to use controllers

---
+++ controllers/auditionsController.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router = express.Router();
+const Audition = require('../models/Audition');
+
+//get auditions
+const getAuditions = async (req, res) => {
+    try {
+      const { location, role } = req.query;
+      const filters = {};
+      if (location) filters.location = location; //if given query params
+      if (role) filters.role = role;
+  
+      const auditions = await Audition.find(filters); //get and filter within constraints of given params
+      res.status(200).json(auditions);
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to retrieve auditions' });
+    }
+  };
+  
+
+// Create a new audition
+const createAudition = async (req, res) => {
+    try {
+      const newAudition = await Audition.create(req.body);
+      res.status(201).json(newAudition);
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to create audition' });
+    }
+  };
+
+// Get a specific audition by ID
+const getAuditionById = async (req, res) => {
+    try {
+      const audition = await Audition.findById(req.params.id);
+      if (!audition) {
+        return res.status(404).json({ error: 'Audition not found' });
+      }
+      res.status(200).json(audition);
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to retrieve audition' });
+    }
+  };
+
+// Update an existing audition
+const updateAudition = async (req, res) => {
+    try {
+      const updatedAudition = await Audition.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      res.status(200).json(updatedAudition);
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to update audition' });
+    }
+  };
+  
+// Delete an audition
+const deleteAudition = async (req, res) => {
+    try {
+      await Audition.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: 'Audition deleted successfully' });
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to delete audition' });
+    }
+  };
+  
+  module.exports = {
+    createAudition,
+    getAuditions,
+    getAuditionById, // Include this in the exported functions
+    updateAudition,
+    deleteAudition,
+  };
+  
--- routes/auditions.js
+++ routes/auditions.js
@@ -1,81 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const Audition = require('../models/Audition');
 
 // /api/auditions
 
-//GET all auditions
-router.get('/', async (req, res) => {
-    try {
-      const { location, gender, role_name } = req.query; //get params from request
-  
-    //if given query parameters, set query to desired param
-      const query = {};
-      if (location) query.location = location;
-      if (gender) query.gender = gender;
-      if (role_name) query.role_name = { $regex: role_name, $options: 'i' };
-  
-      const auditions = await Audition.find(query); //find by the given query
-      res.json(auditions);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  });
-  
+const {
+  createAudition,
+  getAuditions,
+  getAuditionById, // Import the function
+  updateAudition,
+  deleteAudition,
+} = require('../controllers/auditionsController');
 
-//GET a single audition by id
-router.get('/:id', async (req, res) => {
-    try {
-        const audition = await Audition.findById(req.params.id);
-        if (!audition) return res.status(404).json({ message: 'Audition not found' }); //if no audition
-        res.json(audition); //return audition
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-//PUT Update an existing audition
-router.put('/:id', async (req, res) => {
-    try {
-        const updatedAudition = await Audition.findByIdAndUpdate(
-            req.params.id, //find by id
-            { $set: req.body }, //set the data to the data given in the request
-            { new: true }
-        );
-        res.json(updatedAudition); //return the audition as the response
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-//POST a new audition
-router.post('/', async (req, res) => {
-    const { role_name, location, age_range, gender, union_status } = req.body;
-    
-    const newAudition = new Audition({
-        role_name,
-        location,
-        age_range,
-        gender,
-        union_status,
-    });
-
-    try {
-        const audition = await newAudition.save(); //save by inserting the new document into the db
-        res.status(201).json(audition); //return the new audition as json
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-//DELETE delete an audition by id
-router.delete('/:id', async (req, res) => {
-    try {
-        await Audition.findByIdAndDelete(req.params.id);
-        res.json({ message: 'Audition deleted successfully' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
+// Routes using controllers
+router.post('/', createAudition);              // Create a new audition
+router.get('/', getAuditions);                // Get all auditions
+router.get('/:id', getAuditionById);          // Get a single audition by ID
+router.put('/:id', updateAudition);           // Update an audition
+router.delete('/:id', deleteAudition);        // Delete an audition
 
 module.exports = router;
