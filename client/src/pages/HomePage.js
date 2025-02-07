99e209c885702cae2c8c4858147fecfd0dda8a88
unknown <kiowesmith@gmail.com>
2025-02-07 18:55:50 -0500

    clean homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -16,7 +16,7 @@ function HomePage() {
                 </Link> 
             </div>
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" />
+                <img src="https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/47807/large_thumb%402x.jpg" alt="mockup" />
             </div>                
         </div>
         </section>
@@ -32,14 +32,14 @@ function HomePage() {
     {/* Testimonials */}
         <div class="flex flex-row w-full mt-4 bg-black justify-evenly">
             <div class="flex flex-col w-1/4 p-5 my-10 items-center">
-                <img class="w-36 h-auto rounded-full" src="https://t4.ftcdn.net/jpg/02/19/63/31/360_F_219633151_BW6TD8D1EA9OqZu4JgdmeJGg4JBaiAHj.jpg" alt="Rounded avatar" />
+                <img class="w-36 h-auto rounded-full" src="https://avatarfiles.alphacoders.com/343/thumb-1920-343061.jpg" alt="Rounded avatar" />
                 <h4 class="text-white font-bold py-5 text-xl text-wrap">"I can’t believe how fast I booked a job! There are auditions on Backstage I don’t find anywhere else."</h4>
-                <h4 class="text-white font-bold text-lg">Jonathan Muney</h4>
+                <h4 class="text-white font-bold text-lg">Leonard Brown</h4>
             </div>
             <div class="flex flex-col w-1/4 p-5 my-10 items-center">
-                <img class="w-36 h-auto rounded-full" src="https://t4.ftcdn.net/jpg/02/19/63/31/360_F_219633151_BW6TD8D1EA9OqZu4JgdmeJGg4JBaiAHj.jpg" alt="Rounded avatar" />
+                <img class="w-36 h-auto rounded-full" src="https://avatarfiles.alphacoders.com/177/thumb-1920-177013.jpg" alt="Rounded avatar" />
                 <h4 class="text-white font-bold py-5 text-xl text-wrap">"It has been a nonstop whirlwind of auditions, callbacks, bookings, and work. I’m never going to stop using Backstage"</h4>
-                <h4 class="text-white font-bold text-lg">Jonathan Muney</h4>
+                <h4 class="text-white font-bold text-lg">Jackie Muney</h4>
             </div>
         </div>
     {/* Featured Jobs */}
@@ -50,31 +50,31 @@ function HomePage() {
             <div class="flex flex-col md:flex-row mt-8 justify-evenly">
                 {/* card 1 */}
                 <div class="flex flex-col bg-white border border-gray-200 shadow rounded-3xl p-8 w-1/4 items-start">
-                    <Link to="/job-board" class="inline-flex px-1 py-1 text-xs font-xs text-center bg-black text-white rounded-full">
-                            New York, NY
+                    <Link target='_blank' to="https://www.exploretalent.com/auditions/horror-short-film-casting-in-denver-paid-denver-co-usa-80247_1668865" class="inline-flex px-1 py-1 text-xs font-xs text-center bg-black text-white rounded-full">
+                            Denver, CO
                     </Link>
                     <div class="flex flex-col">
-                        <Link to="/job-board">
-                            <h1 class="text-2xl font-bold my-5 hover:text-blue-500">Untitled Dramedy</h1>
+                        <Link target='_blank' to="https://www.exploretalent.com/auditions/horror-short-film-casting-in-denver-paid-denver-co-usa-80247_1668865">
+                            <h1 class="text-2xl font-bold my-5 hover:text-blue-500">Horror Short Film Casting in Denver - Paid</h1>
                         </Link>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
+                        <p>The story is still in development, and we're excited to shape it with inspiration from the cast we select. Shoot Details: Duration: 3–4 days (exact dates TBD) Tentative schedule: Between January 3–15, 2025</p> 
                     </div>
-                    <Link to="/job-board" class=" mt-3 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-blue-500 text-white border border-gray-300 rounded-lg hover:bg-blue-800">
+                    <Link target='_blank' to="https://www.exploretalent.com/auditions/horror-short-film-casting-in-denver-paid-denver-co-usa-80247_1668865" class=" mt-3 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-blue-500 text-white border border-gray-300 rounded-lg hover:bg-blue-800">
                             View
                     </Link>
                 </div>
                 {/* card 2 */}
                 <div class="flex flex-col bg-white border border-gray-200 shadow rounded-3xl p-8 w-1/4 items-start">
-                    <Link to="/job-board" class="inline-flex px-1 py-1 text-xs font-xs text-center bg-black text-white rounded-full">
-                            New York, NY
+                    <Link target='_blank' to="https://castingt.com/auditions/major-skincare-brand-campaign-casting-models" class="inline-flex px-1 py-1 text-xs font-xs text-center bg-black text-white rounded-full">
+                            Los Angeles, CA
                     </Link>
                     <div class="flex flex-col">
-                        <Link to="/job-board">
-                            <h1 class="text-2xl font-bold my-5 hover:text-blue-500">Untitled Horror</h1>
+                        <Link target='_blank' to="https://castingt.com/auditions/major-skincare-brand-campaign-casting-models">
+                            <h1 class="text-2xl font-bold my-5 hover:text-blue-500">Major Skincare Brand Campaign Casting Models</h1>
                         </Link>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
+                        <p>Casting a major skincare campaign seeking to feature pairs (couples, friends, siblings). Note: Emphasis on diversity and authenticity.</p> 
                     </div>
-                    <Link to="/job-board" class=" mt-3 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-blue-500 text-white border border-gray-300 rounded-lg hover:bg-blue-800">
+                    <Link target='_blank' to="https://castingt.com/auditions/major-skincare-brand-campaign-casting-models" class=" mt-3 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-blue-500 text-white border border-gray-300 rounded-lg hover:bg-blue-800">
                             View
                     </Link>
                 </div>
